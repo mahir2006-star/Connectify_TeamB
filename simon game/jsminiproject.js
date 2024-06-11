ffb58a284140ceed33837db4825adabc36fcd6f2
Mahir Jain <mahirjain2006@gmail.com>
2024-06-11 11:16:43 +0300

    Add files via upload

--- simon game/jsminiproject.js
+++ simon game/jsminiproject.js
@@ -1,99 +1,70 @@
-
-let h3=document.querySelector("h3");
-let h2=document.querySelector("h2");
-let gameseq=[];
-let userseq=[];
-let level=0;
-let started=false;
-
-let btns =["yellow","red","purple","green"];
-document.addEventListener("keypress",()=>{  
-    if(started==false)
-        {
-           //Omitted code here
-           //Write your code here
-        }
-})
-
-function gameFlash(btn){
-    btn.classList.add("flash");
-    setTimeout(function(){
-        //Omitted code here
-           //Write your code here
-    },400);
-}
-
-
-function userFlash(btn){
-    btn.classList.add("userflash");
-    setTimeout(function(){
-       //Omitted code here
-           //Write your code here
-    },300);
-}
-
-function levelUp(){
-   //Omitted code here
-           //Write your code here
-    h3.innerText=`Level = ${level}`;
-    // random button choose
-    let randInx= Math.floor(Math.random()*4);
-    let randColor=btns[randInx];
-    let randbtn =document.querySelector(`.${randColor}`);
-  
-   //Omitted code here
-  //Write your code here
-}
-
-
-
-
-
-function checkAns(idx){
-    // let idx=level-1;
-    
-    if(userseq[idx]===gameseq[idx])
-    {
-        if(userseq.length===gameseq.length)
-            {
-               //Omitted code here
-           //Write your code here
-            }
-        console.log("correct");
+let h3 = document.querySelector("h3");
+let h2 = document.querySelector("h2");
+let gameseq = [];
+let userseq = [];
+let level = 0;
+let started = false;
+let btns = ["yellow", "red", "purple", "green"];
+document.addEventListener("keypress", () => {
+    if (started == false) {
+        started = true;
+        h3.innerText = `Level = ${level}`;
+        levelUp();
     }
-    else
-    {
-       
-        document.querySelector("body").style.backgroundColor="red";
-        setTimeout(function(){
-            document.querySelector("body").style.backgroundColor="white";
-        },200);
+});
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 400);
+}
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function() {
+        btn.classList.remove("userflash");
+    }, 300);
+}
+function levelUp() {
+    userseq = [];
+    level++;
+    h3.innerText = `Level = ${level}`;
+    let randInx = Math.floor(Math.random() * 4);
+    let randColor = btns[randInx];
+    gameseq.push(randColor);
+    let randbtn = document.querySelector(`.${randColor}`);
+    gameFlash(randbtn);
+}
+function checkAns(idx) {
+    if (userseq[idx] === gameseq[idx]) {
+        if (userseq.length === gameseq.length) {
+            setTimeout(levelUp, 1000);
+        }
+        console.log("correct");
+    } else {
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 200);
         
-        h3.innerHTML=`Game Over ! Your score was <b>${level}</b><br> Press any key to start again`;
+        h3.innerHTML = `Game Over ! Your score was <b>${level}</b><br> Press any key to start again`;
         reset();
     }
 }
-
-function btnPress()
-{
-    let btn =this;
-    userFlash(this);
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
     let userColor = this.classList[1];
-    // console.log(userColor);
-   
-    console.log(gameseq);
-    //Omitted code here
-           //Write your code here
+    userseq.push(userColor);
+    checkAns(userseq.length - 1);
 }
-let allBtns= document.querySelectorAll('.btn');
-for(let btn of allBtns)
-{
-    //Omitted code here
-           //Write your code here
+let allBtns = document.querySelectorAll('.btn');
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
-
-function reset()
-{
-    //Omitted code here
-           //Write your code here
+function reset() {
+    gameseq = [];
+    userseq = [];
+    level = 0;
+    started = false;
+    h3.innerText = "Press any key to start";
 }
